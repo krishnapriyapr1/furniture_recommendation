f7f37526b38f8692915afc907f8d221b56e728bd
krishnapriyapr1 <144605801+krishnapriyapr1@users.noreply.github.com>
2024-10-02 15:44:30 +0530

    Update settings.py

--- handwash_site/settings.py
+++ handwash_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3rc9^=r)f4s*i-foa3evvg$y=h5!at4deik@ux_&!hrynuwjuk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
